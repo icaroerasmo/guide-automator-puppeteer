08666a3bec1e1b46d2a285077538bf24b776b9ba
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-02-09 20:45:19 -0300

    changes screenshot lib

--- br/com/guide-automator/libs/Utils.js
+++ br/com/guide-automator/libs/Utils.js
@@ -1,5 +1,16 @@
+const webshot = require('webshot');
 module.exports = {
     printsName: (name) => {
         console.log(`My name is ${name}`);
     },
+    screenshot(url, selector, path, callback) {    
+            webshot(
+                url,
+                path,
+                { 
+                    captureSelector: selector
+                },
+                callback
+        );
+    }
 };
--- main.js
+++ main.js
@@ -2,17 +2,11 @@ const App = require('@main/libs/FileReader');
 const u = require('@main/libs/Utils');
 
 var app = require("node-server-screenshot");
-app.fromHTML(
-    'This has been modified by injecting the HTML',
-    "test.png",
-    {inject: {
-        url: "https://docs.npmjs.com/creating-a-package-json-file",
-        selector: {className: "article-head"}
-    }},
-    function(){
-        //an image of the HTML has been saved at ./test.png
+u.screenshot('http://www.penta-code.com', '.tr-container', 'test.png', (err)=>{
+    if(!err) {
+        console.log('took screenshot');
     }
-);
+})
 
 const a = new App();
 a.itWorks();
