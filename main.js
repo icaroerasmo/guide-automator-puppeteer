056687f53973d2c53428379861fc1d1a3e9e769c
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-02-09 19:36:06 -0300

    taking screenshots

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const App = require('@main/libs/FileReader');
 const u = require('@main/libs/Utils');
 
+var app = require("node-server-screenshot");
+app.fromHTML(
+    'This has been modified by injecting the HTML',
+    "test.png",
+    {inject: {
+        url: "https://docs.npmjs.com/creating-a-package-json-file",
+        selector: {className: "article-head"}
+    }},
+    function(){
+        //an image of the HTML has been saved at ./test.png
+    }
+);
+
 const a = new App();
 a.itWorks();
 
