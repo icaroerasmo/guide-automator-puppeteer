2e0022e4ef9b5ed358c23c43173cb801bd2b376b
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-02-10 22:19:40 -0300

    refactor

--- br/com/guide-automator/libs/Utils.js
+++ br/com/guide-automator/libs/Utils.js
@@ -1,26 +1,32 @@
-const puppeteer = require('puppeteer');
+const Interpreter = require('main/libs/Interpreter');
 module.exports = {
     printsName: (name) => {
         console.log(`My name is ${name}`);
     },
-    async screenshot(url, selector, path, callback) {    
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            ]
-        });
-        const page = await browser.newPage();
-        await page.goto(url);
-        await page.evaluate((selector) => {
-            const dom = document.querySelector(selector);
-            if(dom){
-                dom.scrollIntoView();
-            }    
-        }, selector);
-        await page.screenshot({path: path});
-        await browser.close();
-        callback();
+    // async screenshot(url, selector, path, callback) {    
+    //     const browser = await puppeteer.launch({
+    //         headless: true,
+    //         args: [
+    //         '--no-sandbox',
+    //         '--disable-setuid-sandbox',
+    //         ]
+    //     });
+    //     const page = await browser.newPage();
+    //     await page.goto(url);
+    //     await page.evaluate((selector) => {
+    //         const dom = document.querySelector(selector);
+    //         if(dom){
+    //             dom.scrollIntoView();
+    //         }    
+    //     }, selector);
+    //     await page.screenshot({path: path});
+    //     await browser.close();
+    //     callback();
+    // }
+
+    async screenshot(url, selector, path, callback) {  
+        let inst = await Interpreter.instance();
+        inst = await inst.screenshot(url, selector, path, callback);
+        inst.close();
     }
 };
