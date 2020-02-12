9a79b6bd3445475ec6267f6d72c5dfe8a656d0e6
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-02-11 21:00:50 -0300

    finishes proxy logic

--- br/com/guide-automator/libs/interpreter/Interpreter.js
+++ br/com/guide-automator/libs/interpreter/Interpreter.js
@@ -1,8 +1,50 @@
 const InterpreterProxy = require('./InterpreterProxy');
+const puppeteer = require('puppeteer');
 
 class Interpreter extends InterpreterProxy {
+    
     constructor() {
         super();
+    }
+
+    async init() {
+        this.browser = await puppeteer.launch({
+            headless: true,
+            args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            ]
+        });
+        this.page = await this.browser.newPage();
+        console.log("initialized");
+        return this;
+    }
+
+    async close(){
+        console.log("closed");
+        await this.browser.close();
+    }
+
+    async screenshot(url, selector, path) {  
+        if(url){
+            console.log(`URL: ${url}`);
+            console.log(`SELECTOR: ${selector}`);
+            console.log(`PATH: ${path}`);
+            await this.page.goto(url);
+        }
+
+        if(selector){
+            console.log(`SELECTOR: ${selector}`);
+            await this.page.evaluate((selector) => {
+                const dom = document.querySelector(selector);
+                if(dom){
+                    dom.scrollIntoView();
+                }    
+            }, selector);
+        }
+        console.log(`PATH: ${path}`);
+        await this.page.screenshot({path: path});
+        return this;
     }
 }
 
--- br/com/guide-automator/libs/interpreter/InterpreterProxy.js
+++ br/com/guide-automator/libs/interpreter/InterpreterProxy.js
@@ -1,5 +1,3 @@
-const puppeteer = require('puppeteer');
-
 class InterpreterProxy {
 
     browser = null;
@@ -7,46 +5,6 @@ class InterpreterProxy {
 
     constructor() {
         this.parseFuncs();
-    }
-
-    async $init() {
-        this.browser = await puppeteer.launch({
-            headless: true,
-            args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            ]
-        });
-        this.page = await this.browser.newPage();
-        console.log("initialized");
-        return this;
-    }
-
-    async $close(){
-        console.log("closed");
-        await this.browser.close();
-    }
-
-    async $screenshot(url, selector, path) {  
-        if(url){
-            console.log(`URL: ${url}`);
-            console.log(`SELECTOR: ${selector}`);
-            console.log(`PATH: ${path}`);
-            await this.page.goto(url);
-        }
-
-        if(selector){
-            console.log(`SELECTOR: ${selector}`);
-            await this.page.evaluate((selector) => {
-                const dom = document.querySelector(selector);
-                if(dom){
-                    dom.scrollIntoView();
-                }    
-            }, selector);
-        }
-        console.log(`PATH: ${path}`);
-        await this.page.screenshot({path: path});
-        return this;
     }
 
     async proxy(methodName, args){
@@ -69,15 +27,20 @@ class InterpreterProxy {
     }
 
     parseFuncs() {
-        let methods = Object.getOwnPropertyNames(InterpreterProxy.prototype).
-            filter(m => m.startsWith('$'));
+        let thisPrototype = Object.getPrototypeOf(this);
+        let methods = Object.getOwnPropertyNames(thisPrototype).
+            filter(m => m !== 'constructor');
         console.log(`METHODS: ${JSON.stringify(methods)}`);
         for(let m of methods) {
             console.log(`SINGLE METHOD: ${m}`);
-            let methodName = m.replace('$', '');
-            InterpreterProxy.prototype[methodName] = async (...args) => {
-                return await this.proxy(m, args)}
+            const methodName = '_'+m;
+            thisPrototype[methodName] = thisPrototype[m];
+            thisPrototype[m] =
+             async (...args) => {
+                return await this.proxy(methodName, args)
+            }
         }
+        Object.setPrototypeOf(this, thisPrototype);
     }
 }
 
