9934b28d28a1f7aff9231e76a97ffa0e52eb2add
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-02-24 14:25:55 -0300

    resizes images to put in pdf

---
+++ br/com/guide-automator/libs/ImageResizer.js
@@ -0,0 +1,17 @@
+/* RESIZE OPTIMIZE IMAGES */
+const Jimp = require('jimp');
+
+/**
+ * Resize + optimize images.
+ *
+ * @param Array images An array of images paths.
+ * @param Number width A number value of width e.g. 1920.
+ * @param Number height Optional number value of height e.g. 1080.
+ * @param Number quality Optional number value of quality of the image e.g. 90.
+ */
+module.exports = async (imgPath, width, quality) => {
+    const image = await Jimp.read(imgPath);
+    await image.quality(quality);
+    await image.resize(width, Jimp.AUTO);
+    await image.writeAsync(imgPath);
+};
