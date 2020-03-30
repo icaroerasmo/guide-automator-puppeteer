5a563c68ec5ac87d0610bca1c5c938c9df4d4ade
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-03-30 11:05:36 -0300

    Bug fix

--- br/com/guide-automator/libs/Util.js
+++ br/com/guide-automator/libs/Util.js
@@ -3,11 +3,11 @@ module.exports = {
         return str.substr(0, begin) + replacement + str.substr(end);
     },
     splitCodeIntoLines(code) {
-        return code.split('\n').filter(w => w !== '' && w !== ' ');
+        return code.split('\n').filter(w => !w.match(/^\s*$/g));
     },
     splitCommandLine: (line) => {
         return line.split(/('.*?'|".*?"|\S+)/g).
-            filter(w => w != null && w !== '' && w !== ' ').
+            filter(w => w != null && !w.match(/^\s*$/g)).
             map(w => w.replace(/"/g, "").replace(/'/g, ""));
     },
 };
