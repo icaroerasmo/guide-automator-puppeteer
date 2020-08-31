12ba4a22a6db29520ff31c079785a579e27721be
Icaro Erasmo <erasmo.iesb@gmail.com>
2020-08-31 04:51:24 -0300

    Adding git dependencie

--- guide-automator.sh
+++ guide-automator.sh
@@ -1,9 +1,9 @@
-GUIDE_AUTOMATOR_LIBS=$(pwd)/resources/libs
-export PATH=$GUIDE_AUTOMATOR_LIBS/wkhtmltox/bin:$PATH
+export GUIDE_AUTOMATOR_LIBS=$(pwd)/resources/libs
+export PATH=$GUIDE_AUTOMATOR_LIBS/git:$GUIDE_AUTOMATOR_LIBS/node/v12.15.0/bin:$GUIDE_AUTOMATOR_LIBS/wkhtmltox/bin:$PATH
 
 if [ ! -e "./node_modules" ]; then
     echo 'Installing dependencies...'
-    $GUIDE_AUTOMATOR_LIBS/node/v12.15.0/bin/npm i  > /dev/null 2>&1
+    npm i  > /dev/null 2>&1
 fi
 
-$GUIDE_AUTOMATOR_LIBS/node/v12.15.0/bin/node main $@
+node main $@
