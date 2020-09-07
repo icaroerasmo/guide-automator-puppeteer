b46e8a438eab9fa8b55aeb79a3cffe6cbfa2e790
Ícaro Erasmo <erasmo.iesb@gmail.com>
2020-09-07 01:54:16 -0300

    script bug repairs

--- guide-automator.sh
+++ guide-automator.sh
@@ -1,5 +1,13 @@
-export GUIDE_AUTOMATOR_LIBS=$(pwd)/resources/libs
-export PATH=$GUIDE_AUTOMATOR_LIBS/node/v12.15.0/bin:$GUIDE_AUTOMATOR_LIBS/wkhtmltox/bin:$PATH
+#!/usr/bin/env bash
+
+FULL_PATH_SCRIPT=$(readlink -f "$0")
+CONTAINING_FOLDER=$(dirname "$FULL_PATH_SCRIPT")
+GUIDE_AUTOMATOR_LIBS=$CONTAINING_FOLDER/resources/libs
+PATH=$GUIDE_AUTOMATOR_LIBS/node/v12.15.0/bin:$PATH
+PATH=$GUIDE_AUTOMATOR_LIBS/wkhtmltox/bin:$PATH
+PATH=$GUIDE_AUTOMATOR_LIBS/ffmpeg:$PATH
+
+export PATH
 
 if ! [ -x "$(command -v git)" ]; then
   echo 'Error: git is not installed.' >&2
